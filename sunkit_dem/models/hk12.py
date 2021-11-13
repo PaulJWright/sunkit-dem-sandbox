9135a65fa3df77ba03c08e3fc8a330e68c8c5d6b
= <paul@pauljwright.co.uk>
2021-11-13 13:31:36 +0000

    HK12 nearly working; broadcast issue

--- sunkit_dem/models/hk12.py
+++ sunkit_dem/models/hk12.py
@@ -7,16 +7,23 @@ import numpy as np
 import astropy.units as u
 from scipy.interpolate import splrep, splev
 from sunkit_dem import GenericModel
-from synthesizAR.instruments.sdo import _TEMPERATURE_RESPONSE
+
+# from synthesizAR.instruments.sdo import _TEMPERATURE_RESPONSE
 
 # Clone this repo and replace path below: https://github.com/ianan/demreg
-sys.path.append('/path/to/demreg/python')
+sys.path.append("/Users/pwright/Documents/personal/sunkit-dem/demreg/python/")
 from dn2dem_pos import dn2dem_pos
 
 
 class HK12Model(GenericModel):
-
-    def _model(self, alpha=1.0, increase_alpha=1.5, max_iterations=10, guess=None, use_em_loci=False):
+    def _model(
+        self,
+        alpha=1.0,
+        increase_alpha=1.5,
+        max_iterations=10,
+        guess=None,
+        use_em_loci=False,
+    ):
         errors = np.array([c.uncertainty.array.squeeze() for c in self.data]).T
         dem, edem, elogt, chisq, dn_reg = dn2dem_pos(
             self.data_matrix.value.T,
@@ -30,19 +37,25 @@ class HK12Model(GenericModel):
             dem_norm0=guess,
             gloci=use_em_loci,
         )
-        dem_unit = self.data_matrix.unit / self.kernel_matrix.unit / self.temperature_bin_edges.unit
+        dem_unit = (
+            self.data_matrix.unit
+            / self.kernel_matrix.unit
+            / self.temperature_bin_edges.unit
+        )
         dem = dem.T * dem_unit
         uncertainty = edem.T * dem_unit
         em = dem * np.diff(self.temperature_bin_edges)
-        T_error_upper = self.temperature_bin_centers * (10**elogt -1 )
-        T_error_lower = self.temperature_bin_centers * (1 - 1 / 10**elogt)
-        return {'dem': dem,
-                'uncertainty': uncertainty,
-                'em': em,
-                'temperature_errors_upper': T_error_upper,
-                'temperature_errors_lower': T_error_lower,
-                'chi_squared': np.atleast_1d(chisq)}
+        T_error_upper = self.temperature_bin_centers * (10 ** elogt - 1)
+        T_error_lower = self.temperature_bin_centers * (1 - 1 / 10 ** elogt)
+        return {
+            "dem": dem,
+            "uncertainty": uncertainty,
+            "em": em,
+            "temperature_errors_upper": T_error_upper,
+            "temperature_errors_lower": T_error_lower,
+            "chi_squared": np.atleast_1d(chisq),
+        }
 
     @classmethod
     def defines_model_for(self, *args, **kwargs):
-        return kwargs.get('model') == 'hk12'
+        return kwargs.get("model") == "hk12"
